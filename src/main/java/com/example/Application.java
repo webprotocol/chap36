6d210d2109f148d6f47002a7168d8d8fffdbf003
protocls <protocols@example.com>
2023-06-19 12:04:14 +0900

    Application 수정됨

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		System.out.println("###################");
+		System.out.println("# Spring Boot START");
+		System.out.println("###################");
 		SpringApplication.run(Application.class, args);
 	}
 
